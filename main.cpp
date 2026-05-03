99127e68e35ea3adbd0305e225935ed76210ec37
VolodymyrBryzh <volodymyr.bryzh@gmail.com>
2026-05-04 01:22:05 +0300

    feat: integrate raylib graphics library and update build configuration

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "raylib.h"
-#include <vector>
 #include <cmath>
 
 int main() {
@@ -10,9 +9,9 @@ int main() {
     InitWindow(screenWidth, screenHeight, "Survival Engine C++ | Visual Studio");
 
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 20.0f, 20.0f, 20.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.position = Vector3{ 20.0f, 20.0f, 20.0f };
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
